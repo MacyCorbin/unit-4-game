f6622eafc29dca56f4b3d194760eaa3749e58527
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-03-26 22:50:21 -0400

    yes

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # unit-4-game
+
+
+#---------- Files ----------#
+
+* index.html
+* README.md
+* Assets
+    * css
+        * reset.css
+        * style.css
+    * images
+        * Ginny
+        * Harry
+        * hermione
+        * Ron
+        * Room_of _Requirement
+        * Harry Potter font
+    * javascript
+        * game.js
+    * sounds
+        * spellSound
+
+##---------- Gameplay ----------##
+
+* On the initial screen there are 4 characters in a row: Harry, Hermione, Ron, and Ginny.
+* Each character starts off with total health.
+* First, the user will choose their character to play and then select an opponent from the remaining three characters.
+
+* When the cast spell button is selected the duel begins!
+* Several things are associated with the click of the cast spell button
+    * The sound of spells from wands
+    * The spell damage to the user's character health and the opponent's health
+    * The type of spell cast
+    * The spell damage worth
+
+* The game will continue until the user's character looses, game would restart, or opponent is defeated
+* If the opponent is defeated, the user chooses the next opponent out of the remaining two
+* The game continues until all opponent characters are defeated or if the user's character looses
+* When the game ends, it will reset.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,50 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     //============= Variables =============//
 
     var spellSound
-    var gameStart = false;        
-    var gameEnd = false; 
+    var startGame = false;
+    var gameEnd = false;
 
     //============= Character Objects =============//
 
-var characters = {
+    var characters = {
 
-    "Harry":{
-        name: "Harry",
-        health: 230,
-        attack: Math.floor((Math.random() * 30) + 0),
-        imageUrl: "assets/images/Harry.jpg",
-        "enemyAttackBack": 20,  
-    },
-
-
-    "Hermione":{
-        name: "Hermione",
-        health: 240,
-        attack: Math.floor((Math.random() * 30) + 0),
-        imageUrl: "assets/images/hermione.jpg",
-        enemyAttackBack: 20,  
-    },
+        "Harry": {
+            name: "Harry",
+            health: 230,
+            attack: 20,
+            imageUrl: "assets/images/Harry.jpg",
+            "enemyAttackBack": 20,
+        },
 
 
-    "Ron":{
-        name: "Ron",
-        health: 220,
-        attack: Math.floor((Math.random() * 30) + 0),
-        imageUrl: "assets/images/Ron.png",
-        enemyAttackBack: 25,  
-    },
+        "Hermione": {
+            name: "Hermione",
+            health: 240,
+            attack: 20,
+            imageUrl: "assets/images/hermione.jpg",
+            enemyAttackBack: 20,
+        },
 
-    "Ginny":{
-        name: "Ginny",
-        health: 230,
-        attack: Math.floor((Math.random() * 30) + 0),
-        imageUrl: "assets/images/Ginny.jpg",
-        enemyAttackBack: 25,  
-    },
 
-};
+        "Ron": {
+            name: "Ron",
+            health: 220,
+            attack: 20,
+            imageUrl: "assets/images/Ron.png",
+            enemyAttackBack: 25,
+        },
+
+        "Ginny": {
+            name: "Ginny",
+            health: 230,
+            attack: 20,
+            imageUrl: "assets/images/Ginny.jpg",
+            enemyAttackBack: 25,
+        },
+
+    };
 
     var opponents = [];
     var opponent;
@@ -52,44 +52,40 @@ var characters = {
     var defeatedCount = 0;
     var characterTurn = 1;
 
-   
 
-    // var harryHealth = 200
-    // var hermioneHealth = 200
-    // var ronHealth = 200
-    // var ginnyHealth = 200
-   // var userSpell;
-    //var opponentSpell;
 
-    
+    //var userSpell;    //<--------------Here is the variable for adding a random spell later in the code is the math.random and spell array list
+   // var opponentSpell;
 
 
 
-   
-    
+
+
+
+
     //============= Functions =============//
 
 
     //-------  Load Characters to HTML  -------//
 
-    var renderCharacters = function (character, renderArea){
+    var renderCharacters = function (character, renderArea) {
 
-        //var characterDiv = $("<div class='col-lg-3 text-center' >");
+        //var characterDiv = $("<div class='col-lg-3' >");
+
+        var characterName = $("<div class='cardText' class='card-body info'>").text(character.name+" " +character.health);
+        var characterCard = $("<div id = 'userChoice' class='card text-white bg-info' class ='cardBorder'>");
+        var characterBorder = $("<span id ='imgHover'class ='cardBorder'> ");
+        var characterImage = $("<img class='card-img-top' class='cardStyle' alt ='character image' class='cardStyle' id ='character-image'>").attr("src", character.imageUrl);
         
-        var characterName = $("<div class='cardText' class='card-body info'>").text(character.name);
-        var characterCard = $("<div id = 'userChoice' class='card text-white bg-info'>");
-        var characterBorder = $("<span id ='imgHover'> ");
-        var characterImage = $("<img class='card-img-top' class='cardStyle' alt ='character image' class='cardStyle' id ='character-image'>").attr("src",character.imageUrl);
+        characterCard.append(characterBorder).append(characterImage).append(characterName);
 
-        (characterCard).append(characterBorder).append(characterImage).append(characterName);
-       
         $(renderArea).append(characterCard);
     }
 
-    
 
-    var startGame = function(){
-        for (var key in characters){
+
+    var startGame = function () {
+        for (var key in characters) {
             renderCharacters(characters[key], "#character-selection")
         }
     };
@@ -97,41 +93,15 @@ var characters = {
     startGame();
 
 
-   
+
 
 
     // //-------  Update Display  -------//
 
-    var updateCharacters = function (characterObject, areaRender){
+    var updateCharacters = function (characterObject, areaRender) {
         $(areaRender).empty();
-        renderCharacters(characterObject,areaRender);
+        renderCharacters(characterObject, areaRender);
     };
-
-
-
-    // function updateDisplay() {
-
-    //     //Displays health of characters
-        //document.getElementById("harryHealth").innerText = harryHealth;
-        //document.getElementById("hermioneHealth").innerText = hermioneHealth;
-        //document.getElementById("ginnyHealth").innerText = ginnyHealth;
-        //document.getElementById("ronHealth").innerText = ronHealth;
-
-       // document.getElementById("userChoice").innerText = userChoice;
-        //document.getElementById("userPlacement").innerText = userPlacement;
-
-
-       // var audioElement = document.createElement("audio");
-       // audioElement.setAttribute("/assets/sounds/spellSound.mp3");
-        //Displays spells of characters
-        //document.getElementById("userSpell").innerText = userSpell;
-        //document.getElementById("opponentSpell").innerText = opponentSpell;
- //Displays power of spells
-       // document.getElementById("userSpellDamage").innerText = userSpellDamage;
-       // document.getElementById("opponentSpellDamage").innerText = opponentSpellDamage;
-
-
-    // }
 
 
     // //-------  Reset Game  -------//
@@ -139,8 +109,8 @@ var characters = {
     // function resetGame() {
     //     remainingGuesses = numberOfGuesses;
     //     gameStart = false;
-    
-      
+
+
     //     // Show display
     //     updateDisplay();
     // };
@@ -149,9 +119,39 @@ var characters = {
 
 
 
-    //-------  Attack  -------//
 
-    var spellList = [
+    //-------  Choose Players  -------//
+
+
+       $('#userChoice').on('click', function (){
+
+            if (!userChoice){
+                $(this).appendTo('#userPlacement');
+                $(this).find('#imgHover')
+                    .removeId('#imgHover');
+
+
+            }
+
+            updateCharacters();
+        })
+
+
+        
+//-------  Attack  -------//
+
+
+    //attack button click
+
+
+
+    
+
+
+
+
+
+    var spellList = [   //array for a random spell to be chosen
 
         "confundus",
         "stupefy",
@@ -164,57 +164,9 @@ var characters = {
     ]
 
 
-    //attack button click
 
-   
-    
-    
-    //newFunction()
-
-//     document.getElementById("spell-button").addEventListener("click", attackFunction);
-
-// //function newFunction() {
-  
-
-//     function attackFunction() {
-
-//        // spellSound = new sound("/assets/sounds/spellSound.mp3");
-// //        // spellSound.play();
-
-
-//     }
-   
-
-//     // userSpell = Math.floor(Math.random() * (spellList.length));
-//     // opponentSpell = Math.floor(Math.random() * (spellList.length));
-
-    // userSpellDamage = Math.floor((Math.random() * 30) + 0);
-    // opponentSpellDamage = Math.floor((Math.random() * 30) + 0);
-
-
-
-
-
-
-
-
-    //-------  Choose Players  -------//
-
-
-//    $('#userChoice').on('click', function (){
-
-//         if (!userChoice){
-//             $(this).appendTo('#userPlacement');
-//             $(this).find('#imgHover')
-//                 .removeId('#imgHover');
-
-              
-        
-//         }
-
-//         updateDisplay();
-//     })
-
+    //userSpell = Math.floor(Math.random() * (spellList.length));  <------------ a random spell is chosen from the spellList array for the user's character
+    //opponentSpell = Math.floor(Math.random() * (spellList.length));  <------------ a random spell is chosen from the spellList array for the opponents
 
 
 
